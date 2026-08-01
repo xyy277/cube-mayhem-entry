eb190d480a0853e981459d8d219f3082c93e9ea2
代码横扫千行，跨界皆为王 <37684007+xyy277@users.noreply.github.com>
2026-08-02 07:19:28 +0800

    deploy entry: sw.js

---
+++ sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install', function(e){ self.skipWaiting(); });
+self.addEventListener('activate', function(e){ e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', function(){});
